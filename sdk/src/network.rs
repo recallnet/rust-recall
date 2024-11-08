53a3ccc5dbc6b52c82d040f3d39fb47516f98cf1
Ben Wilson <ben@3box.io>
2024-11-07 22:36:16 -0500

    feat: revert testnet changes and add ignition network

--- sdk/src/network.rs
+++ sdk/src/network.rs
@@ -15,17 +15,20 @@ use hoku_signer::SubnetID;
 
 use crate::ipc::subnet::EVMSubnet;
 
-const TESTNET_SUBNET_ID: &str = "/r314159/t410fihazdq4n3v6xs7olrubepndnkbrwyxkzjm5kusa"; // chain ID: 2938118273996536
+const TESTNET_SUBNET_ID: &str = "/r314159/t410fvamrbjioufgzoyojg2x3nwdo26t6xucxoxl47yq"; // chain ID: 2938118273996536
 const LOCALNET_SUBNET_ID: &str = "/r31337/t410fkzrz3mlkyufisiuae3scumllgalzuu3wxlxa2ly"; // chain ID: 4362550583360910
 const DEVNET_SUBNET_ID: &str = "test";
+const IGNITION_SUBNET_ID: &str = "/r314159/t410fihazdq4n3v6xs7olrubepndnkbrwyxkzjm5kusa"; // chain ID: 1678692468424472
 
-const TESTNET_RPC_URL: &str = "https://rpc-testnet-ignition-v1-validator-0.3box.io";
+const TESTNET_RPC_URL: &str = "https://api.n1.hoku.sh";
 const LOCALNET_RPC_URL: &str = "http://127.0.0.1:26657";
+const IGNITION_RPC_URL: &str = "https://rpc-testnet-ignition-v1-validator-0.3box.io";
 
 const RPC_TIMEOUT: Duration = Duration::from_secs(60);
 
-const TESTNET_EVM_RPC_URL: &str = "https://evm-testnet-ignition-v1-validator-0.3box.io";
+const TESTNET_EVM_RPC_URL: &str = "https://evm-api.n1.hoku.sh";
 const LOCALNET_EVM_RPC_URL: &str = "http://127.0.0.1:8645";
+const IGNITION_EVM_RPC_URL: &str = "https://evm-testnet-ignition-v1-validator-0.3box.io";
 
 const TESTNET_EVM_GATEWAY_ADDRESS: &str = "0x77aa40b105843728088c0132e43fc44348881da8";
 const TESTNET_EVM_REGISTRY_ADDRESS: &str = "0x74539671a1d2f1c8f200826baba665179f53a1b7";
@@ -35,14 +38,17 @@ const LOCALNET_EVM_REGISTRY_ADDRESS: &str = "0x74539671a1d2f1c8f200826baba665179
 const LOCALNET_EVM_SUPPLY_SOURCE_ADDRESS: &str = "0xE6E340D132b5f46d1e472DebcD681B2aBc16e57E";
 
 const TESTNET_PARENT_EVM_RPC_URL: &str = "https://api.calibration.node.glif.io/rpc/v1";
-const TESTNET_PARENT_EVM_GATEWAY_ADDRESS: &str = "0xF8Abf46A1114d3B44d18F2A96D850e36FC6Ee94E";
-const TESTNET_PARENT_EVM_REGISTRY_ADDRESS: &str = "0x0bb143a180b61ae6b1872bbf99dBe261A2aDde40";
+const TESTNET_PARENT_EVM_GATEWAY_ADDRESS: &str = "0xe17B86E7BEFC691DAEfe2086e56B86D4253f3294";
+const TESTNET_PARENT_EVM_REGISTRY_ADDRESS: &str = "0xe87AFBEC26f0fdAC69e4256dC1935bEab1e0855E";
 const LOCALNET_PARENT_EVM_RPC_URL: &str = "http://127.0.0.1:8545";
 const LOCALNET_PARENT_EVM_GATEWAY_ADDRESS: &str = "0x9A676e781A523b5d0C0e43731313A708CB607508";
 const LOCALNET_PARENT_EVM_REGISTRY_ADDRESS: &str = "0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1";
+const IGNITION_PARENT_EVM_GATEWAY_ADDRESS: &str = "0xF8Abf46A1114d3B44d18F2A96D850e36FC6Ee94E";
+const IGNITION_PARENT_EVM_REGISTRY_ADDRESS: &str = "0x0bb143a180b61ae6b1872bbf99dBe261A2aDde40";
 
-const TESTNET_OBJECT_API_URL: &str = "https://object-api-testnet-ignition-v1-validator-0.3box.io";
+const TESTNET_OBJECT_API_URL: &str = "https://object-api.n1.hoku.sh";
 const LOCALNET_OBJECT_API_URL: &str = "http://127.0.0.1:8001";
+const IGNITION_OBJECT_API_URL: &str = "https://object-api-testnet-ignition-v1-validator-0.3box.io";
 
 /// Options for [`EVMSubnet`] configurations.
 #[derive(Debug, Clone)]
@@ -73,6 +79,8 @@ pub enum Network {
     Localnet,
     /// Network presets for local development.
     Devnet,
+    /// Network presets for Ignition testnet.
+    Ignition,
 }
 
 impl Network {
@@ -81,7 +89,7 @@ impl Network {
     pub fn init(&self) -> &Self {
         match self {
             Network::Mainnet => set_current_network(FvmNetwork::Mainnet),
-            Network::Testnet | Network::Localnet | Network::Devnet => {
+            Network::Testnet | Network::Localnet | Network::Devnet | Network::Ignition => {
                 set_current_network(FvmNetwork::Testnet)
             }
         }
@@ -95,6 +103,7 @@ impl Network {
             Network::Testnet => Ok(SubnetID::from_str(TESTNET_SUBNET_ID)?),
             Network::Localnet => Ok(SubnetID::from_str(LOCALNET_SUBNET_ID)?),
             Network::Devnet => Ok(SubnetID::from_str(DEVNET_SUBNET_ID)?),
+            Network::Ignition => Ok(SubnetID::from_str(IGNITION_SUBNET_ID)?),
         }
     }
 
@@ -117,6 +126,7 @@ impl Network {
             Network::Mainnet => Err(anyhow!("network is pre-mainnet")),
             Network::Testnet => Ok(Url::from_str(TESTNET_RPC_URL)?),
             Network::Localnet | Network::Devnet => Ok(Url::from_str(LOCALNET_RPC_URL)?),
+            Network::Ignition => Ok(Url::from_str(IGNITION_RPC_URL)?),
         }
     }
 
@@ -126,6 +136,7 @@ impl Network {
             Network::Mainnet => Err(anyhow!("network is pre-mainnet")),
             Network::Testnet => Ok(Url::from_str(TESTNET_OBJECT_API_URL)?),
             Network::Localnet | Network::Devnet => Ok(Url::from_str(LOCALNET_OBJECT_API_URL)?),
+            Network::Ignition => Ok(Url::from_str(IGNITION_OBJECT_API_URL)?),
         }
     }
 
@@ -136,6 +147,7 @@ impl Network {
             Network::Testnet => Ok(reqwest::Url::from_str(TESTNET_EVM_RPC_URL)?),
             Network::Localnet => Ok(reqwest::Url::from_str(LOCALNET_EVM_RPC_URL)?),
             Network::Devnet => Err(anyhow!("network has no parent")),
+            Network::Ignition => Ok(reqwest::Url::from_str(IGNITION_EVM_RPC_URL)?),
         }
     }
 
@@ -146,6 +158,7 @@ impl Network {
             Network::Testnet => Ok(parse_address(TESTNET_EVM_GATEWAY_ADDRESS)?),
             Network::Localnet => Ok(parse_address(LOCALNET_EVM_GATEWAY_ADDRESS)?),
             Network::Devnet => Err(anyhow!("network has no parent")),
+            Network::Ignition => Ok(parse_address(IGNITION_PARENT_EVM_GATEWAY_ADDRESS)?),
         }
     }
 
@@ -156,6 +169,7 @@ impl Network {
             Network::Testnet => Ok(parse_address(TESTNET_EVM_REGISTRY_ADDRESS)?),
             Network::Localnet => Ok(parse_address(LOCALNET_EVM_REGISTRY_ADDRESS)?),
             Network::Devnet => Err(anyhow!("network has no parent")),
+            Network::Ignition => Ok(parse_address(IGNITION_PARENT_EVM_REGISTRY_ADDRESS)?),
         }
     }
 
@@ -179,6 +193,7 @@ impl Network {
             Network::Testnet => Ok(reqwest::Url::from_str(TESTNET_PARENT_EVM_RPC_URL)?),
             Network::Localnet => Ok(reqwest::Url::from_str(LOCALNET_PARENT_EVM_RPC_URL)?),
             Network::Devnet => Err(anyhow!("network has no parent")),
+            Network::Ignition => Ok(reqwest::Url::from_str(TESTNET_PARENT_EVM_RPC_URL)?),
         }
     }
 
@@ -189,6 +204,7 @@ impl Network {
             Network::Testnet => Ok(parse_address(TESTNET_PARENT_EVM_GATEWAY_ADDRESS)?),
             Network::Localnet => Ok(parse_address(LOCALNET_PARENT_EVM_GATEWAY_ADDRESS)?),
             Network::Devnet => Err(anyhow!("network has no parent")),
+            Network::Ignition => Ok(parse_address(IGNITION_PARENT_EVM_GATEWAY_ADDRESS)?),
         }
     }
 
@@ -199,6 +215,7 @@ impl Network {
             Network::Testnet => Ok(parse_address(TESTNET_PARENT_EVM_REGISTRY_ADDRESS)?),
             Network::Localnet => Ok(parse_address(LOCALNET_PARENT_EVM_REGISTRY_ADDRESS)?),
             Network::Devnet => Err(anyhow!("network has no parent")),
+            Network::Ignition => Ok(parse_address(IGNITION_PARENT_EVM_REGISTRY_ADDRESS)?),
         }
     }
 
@@ -209,6 +226,7 @@ impl Network {
             Network::Testnet => Ok(parse_address(TESTNET_EVM_SUPPLY_SOURCE_ADDRESS)?),
             Network::Localnet => Ok(parse_address(LOCALNET_EVM_SUPPLY_SOURCE_ADDRESS)?),
             Network::Devnet => Err(anyhow!("network has no parent")),
+            Network::Ignition => Err(anyhow!("network has no parent")),
         }
     }
 }
@@ -222,6 +240,7 @@ impl FromStr for Network {
             "testnet" => Ok(Network::Testnet),
             "localnet" => Ok(Network::Localnet),
             "devnet" => Ok(Network::Devnet),
+            "ignition" => Ok(Network::Ignition),
             _ => Err(Error::UnknownNetwork.to_string()),
         }
     }
@@ -234,6 +253,7 @@ impl Display for Network {
             Network::Testnet => write!(f, "testnet"),
             Network::Localnet => write!(f, "localnet"),
             Network::Devnet => write!(f, "devnet"),
+            Network::Ignition => write!(f, "ignition"),
         }
     }
 }
