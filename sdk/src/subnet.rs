cc50dac78d2366705608e0a2f5db8e97a10c0e5a
Sander Pick <sanderpick@gmail.com>
2024-12-05 16:30:13 -0800

    chore: cleanup nits
    Signed-off-by: Sander Pick <sanderpick@gmail.com>

--- sdk/src/subnet.rs
+++ sdk/src/subnet.rs
@@ -1,3 +1,6 @@
+// Copyright 2024 Hoku Contributors
+// SPDX-License-Identifier: Apache-2.0, MIT
+
 use hoku_provider::json_rpc::JsonRpcProvider;
 use hoku_provider::TendermintClient;
 use tendermint::chain;
