1c75f3232de1176ee856610cf1dbb20196db2240
Alexei Samokvalov <alexei@sam701.com>
2025-01-09 18:52:25 +0100

    Update contract addresses

--- sdk/src/network.rs
+++ sdk/src/network.rs
@@ -38,16 +38,16 @@ const LOCALNET_PARENT_EVM_REGISTRY_ADDRESS: &str = "0x322813Fd9A801c5507c9de605d
 
 // Ignition
 const TESTNET_RPC_URL: &str = "https://api-ignition-0.hoku.sh";
-const TESTNET_SUBNET_ID: &str = "/r314159/t410f2mjuvgks4vlwbmtfmwohbaqo5v2w5fbcr5zf7li";
+const TESTNET_SUBNET_ID: &str = "/r314159/t410f4f5jzfsftk6zqvcq3c4rrt372itjg6qh5b33dwq";
 const TESTNET_CHAIN_ID: u64 = 2481632;
 const TESTNET_OBJECT_API_URL: &str = "https://object-api-ignition-0.hoku.sh";
 const TESTNET_EVM_RPC_URL: &str = "https://evm-ignition-0.hoku.sh";
 const TESTNET_PARENT_EVM_RPC_URL: &str = "https://api.calibration.node.glif.io/rpc/v1";
-const TESTNET_PARENT_EVM_GATEWAY_ADDRESS: &str = "0xF8Abf46A1114d3B44d18F2A96D850e36FC6Ee94E";
-const TESTNET_PARENT_EVM_REGISTRY_ADDRESS: &str = "0x0bb143a180b61ae6b1872bbf99dBe261A2aDde40";
+const TESTNET_PARENT_EVM_GATEWAY_ADDRESS: &str = "0x88b497225c33dDF92A4F94C0C82a20044e68Bc9E";
+const TESTNET_PARENT_EVM_REGISTRY_ADDRESS: &str = "0x4665468636B81481adac664993782609a1F3cB30";
 const TESTNET_EVM_GATEWAY_ADDRESS: &str = "0x77aa40b105843728088c0132e43fc44348881da8";
 const TESTNET_EVM_REGISTRY_ADDRESS: &str = "0x74539671a1d2f1c8f200826baba665179f53a1b7";
-const TESTNET_EVM_SUPPLY_SOURCE_ADDRESS: &str = "0x20d8a696091153c4d4816ba1fdefe113f71e0905";
+const TESTNET_EVM_SUPPLY_SOURCE_ADDRESS: &str = "0xA7D1A73173A6854b7C8c1DF4d22a1d39BfcFb021";
 
 #[derive(Debug, Clone)]
 pub struct NetworkConfig {
