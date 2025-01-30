bae301ff70e086c84d758b2e122d8e6f57320921
Joe Wagner <joewagner@users.noreply.github.com>
2025-01-30 08:50:12 -0700

    feat: first integration test written (#189)
    fixes: https://github.com/recallnet/rust-hoku/issues/186
    
    This PR introduces the first few integration tests. These tests are
    ignored by default so CI still passes. Once we have finished
    https://github.com/recallnet/rust-hoku/issues/183 we can remove the
    `ignore`s and run these on PRs.
    
    In the meantime to run against localnet do:
    - start localnet
    - run `TEST_TARGET_NETWORK=localnet
    HOKU_PRIVATE_KEY=0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba
    cargo test --test '*' -- --nocapture --ignored`
    
    The above private key is anvil account 5 
    
    You can also run these against testnet if you have a private key with
    funds+tokens+credits by setting `TEST_TARGET_NETWORK=testnet`

---
+++ sdk/tests/common/mod.rs
@@ -0,0 +1,40 @@
+use hoku_sdk::network::{Network, NetworkConfig};
+use std::env;
+
+pub fn setup() {
+    // TODO
+}
+
+pub fn get_network() -> NetworkConfig {
+    let net_name = match env::var("TEST_TARGET_NETWORK") {
+        Ok(network) => network,
+        Err(e) => panic!("cannot get test target network {}", e),
+    };
+
+    match net_name.as_str() {
+        "localnet" => {
+            Network::Localnet.init();
+            Network::Localnet.get_config()
+        }
+        "testnet" => {
+            Network::Testnet.init();
+            Network::Testnet.get_config()
+        }
+        "mainnet" => {
+            Network::Mainnet.init();
+            Network::Mainnet.get_config()
+        }
+        _ => panic!("cannot get test target network config"),
+    }
+}
+
+pub fn get_runner_secret_key() -> String {
+    match env::var("HOKU_PRIVATE_KEY") {
+        Ok(sk) => sk,
+        Err(e) => panic!("cannot get secret key for test runner {}", e),
+    }
+}
+
+pub fn get_runner_auth_token() -> String {
+    env::var("HOKU_AUTH_TOKEN").unwrap_or_default()
+}
